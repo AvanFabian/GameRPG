29d433311e7c1a66934c3cebef137efe852a9fa9
AvanFabian <avanfabiandaniswara@gmail.com>
2023-03-24 06:45:53 +0700

    Add Background image

--- CrossTheRoad.py
+++ CrossTheRoad.py
@@ -14,57 +14,93 @@ WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
 
 
+# Class untuk menampilkan layar
 class Game:
     TICK_RATE = 60
 
-    def __init__(self, title, width, height):
+    def __init__(self, image_path, title, width, height):
         self.title = title
         self.width = width
         self.height = height
 
-        # menampilkan screen
+        # make game screen
         self.game_screen = pg.display.set_mode((width, height))
-        # mengisi background saat awal permainan dengan warna putih
+
+        # fill screen with white color
         self.game_screen.fill(WHITE_COLOR)
         pg.display.set_caption(title)
+
+        # add background image
+        background_image = pg.image.load(image_path)
+        # change size of image to fit the width and height
+        self.image = pg.transform.scale(background_image, (width, height))
 
     def run_game_loop(self):
         is_game_over = False
         direction = 0
 
-        # membuat instance dari class PlayerCharacter
-        player_character = PlayerCharacter('asset/musuh2.png', 375, 700, 50, 50)
+        # membuat instance dari class PlayerCharacter dan EnemyCharacter serta harta karun untuk menampilkan gambar
+        # gambar, posisi koordinat x, posisi koordinat x, width karakter, height karakter
+        player_character = PlayerCharacter('asset/pemain.png', 375, 700, 50, 50)
+        enemy_character = EnemyCharacter('asset/musuh2.png', 225, 400, 50, 50)
+        one_piece = GameObject('asset/onepiece.png', 400, 50, 50, 50)
 
         while not is_game_over:
-            for event in pg.event.get():
+            for event in pg.event.get():  # mengambil event dari keyboard
                 if event.type == QUIT:
                     is_game_over = True
                 # jika tombol ditekan
-                elif event.type == pg.KEYDOWN:  # dictionary: type=key, pg.KEYDOWN=value
-                    if event.key == pg.K_UP:  # y-position berubah ke atas
+                elif event.type == pg.KEYDOWN:
+                    # y-position berubah ke atas
+                    if event.key == pg.K_UP:
                         direction = 1
-                    elif event.key == pg.K_DOWN:  # y-position berubah ke bawah
+                        # y-position berubah ke bawah
+                    elif event.key == pg.K_DOWN:
                         direction = -1
-                    elif event.key == pg.K_ESCAPE:  # keluar dari game
+                        # x-position berubah ke kiri
+                    elif event.key == pg.K_LEFT:
+                        direction = 2
+                        # x-position berubah ke kanan
+                    elif event.key == pg.K_RIGHT:
+                        direction = -2
+                        # keluar dari game
+                    elif event.key == pg.K_ESCAPE:
                         is_game_over = True
                 # jika tombol dilepas
-                elif event.type == pg.KEYUP: # dictionary: type=key, pg.KEYUP=value
-                    if event.key == pg.K_UP or event.key == pg.K_DOWN: # jika tombol atas atau bawah dilepas
+                elif event.type == pg.KEYUP:
+                    # jika tombol atas atau bawah dilepas, maka karakter berhenti
+                    if event.key == pg.K_UP or event.key == pg.K_DOWN:
                         direction = 0
                 print(event)
 
             # me-refresh background tiap kali karakter berjalan
             self.game_screen.fill(WHITE_COLOR)
-            # menambahkan nilai direction ke method move di class player_character
-            player_character.move(direction)
-            # menampilkan screen game | juga menggunakan method didalam class player_character
-            player_character.draw(self.game_screen)
+            self.game_screen.blit(self.image, (0, 0))
 
-            pg.display.update()  # update layar
+            # menambahkan nilai direction ke method move di class player_character
+            player_character.move(direction, self.height)
+            # menambahkan nilai width ke method move di class enemy_character
+            enemy_character.move(self.width)
+
+            # menampilkan player, enemy, dan harta karun
+            player_character.draw(self.game_screen)
+            enemy_character.draw(self.game_screen)
+            one_piece.draw(self.game_screen)
+
+            # collision detection
+            if player_character.detection_collision(enemy_character):
+                is_game_over = True
+                print("Game Over")
+            elif player_character.detection_collision(one_piece):
+                is_game_over = True
+                print("You Win")
+
+            # update layar
+            pg.display.update()
             clock.tick(self.TICK_RATE)
 
 
-# membuat class untuk karakter
+# Superclass for character
 class GameObject:
     def __init__(self, image_path, x, y, width, height):
         self.x_pos = x
@@ -73,32 +109,85 @@ class GameObject:
         self.width = width
         self.height = height
 
-        object_image = pg.image.load(image_path)  # mengambil gambar
-        self.image = pg.transform.scale(object_image, (width, height))  # mengubah ukuran gambar
+        # take image from path
+        object_image = pg.image.load(image_path)
+        # change size of image to fit the width and height
+        self.image = pg.transform.scale(object_image, (width, height))
 
-    # method untuk menampilkan gambar
+    # method to draw object
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
 
 
-class PlayerCharacter(GameObject):  # subclass
+# Player  Character Class
+class PlayerCharacter(GameObject):
 
-    SPEED = 10  # KECEPATAN DALAM FRAME PER SECOND
+    # Define speed variable
+    SPEED = 10
 
     def __init__(self, image_path, x, y, width, height):
         super().__init__(image_path, x, y, width, height)
 
-    def move(self, direction):
-        # direction adalah -1 untuk ke bawah, 1 untuk ke atas
-        if direction > 0:  # ke atas
+    def move(self, direction, max_height):
+        # direction = 1 is up, -1 is down, 2 is left, -2 is right
+        # (0,0) of pygame is at the top left of the screen, so y_pos will decrease when moving up and vice versa
+        if direction == 1:  # when up key is pressed
             self.y_pos -= self.SPEED
-        elif direction < 0:  # ke bawah
+            if self.y_pos <= 0:  # top boundary
+                self.y_pos = 0
+        elif direction == -1:  # when down key is pressed
             self.y_pos += self.SPEED
+            if self.y_pos >= max_height - 70:  # bottom boundary
+                self.y_pos = max_height - 70
+        elif direction == 2:  # when left key is pressed
+            self.x_pos -= self.SPEED
+            if self.x_pos <= 0:  # left boundary
+                self.x_pos = 0
+        elif direction == -2:  # when right key is pressed
+            self.x_pos += self.SPEED
+            if self.x_pos >= max_height - 70:  # right boundary
+                self.x_pos = max_height - 70
+
+    def detection_collision(self, other_body):
+        # if player position in y axis + player height is less than enemy position in y axis
+        if self.y_pos > other_body.y_pos + other_body.height:
+            return False
+        # if player position in y axis is greater than enemy position in y axis + enemy height
+        elif self.y_pos + self.height < other_body.y_pos:
+            return False
+
+        # if player position in x axis + player width is less than enemy position in x axis
+        if self.x_pos > other_body.x_pos + other_body.width:
+            return False
+        # if player position in x axis is greater than enemy position in x axis + enemy width
+        elif self.x_pos + self.width < other_body.x_pos:
+            return False
+
+        # if none of the above is true, then there is a collision
+        return True
+
+
+# Enemy Character Class & Bounds Checking
+class EnemyCharacter(GameObject):
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, max_width):  # max_width param is the width of the screen
+        # if x position is less than 20 or at the left edge, 20 is the left boundary
+        if self.x_pos <= 20:  # left boundary
+            self.SPEED = abs(self.SPEED)  # change speed to positive value
+        # if x position is greater than max_width - 70 or at the right edge, 70 is the right boundary
+        elif self.x_pos >= max_width - 70:  # right boundary
+            self.SPEED = -abs(self.SPEED)  # change speed to negative value
+
+        self.x_pos += self.SPEED  # move enemy character
 
 
 pg.init()
 # buat instance game
-new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game = Game('asset/background.png', SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
 new_game.run_game_loop()
 
 # keluar dari game
